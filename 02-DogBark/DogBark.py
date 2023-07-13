74c407a0033d9072e524965ae93d2740b3a362c7
Matt Boutell <boutell@gmail.com>
2023-07-13 10:59:11 -0400

    Finished 02

--- 02-DogBark/DogBark.py
+++ 02-DogBark/DogBark.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 import sys
 
@@ -17,34 +19,52 @@ def main():
     pygame.display.set_caption("Text, Sound, and an Image")
 
     # Prepare the image
-    # TODO 1: Create an image with the 2dogs.JPG image
-    # TODO 3: Scale the image to be the size (IMAGE_SIZE, IMAGE_SIZE)
+    # DONE 1: Create an image with the 2dogs.JPG image
+    image1 = pygame.image.load("2dogs.JPG")
+
+    # DONE 3: Scale the image to be the size (IMAGE_SIZE, IMAGE_SIZE)
+    image1 = pygame.transform.scale(image1, (IMAGE_SIZE, IMAGE_SIZE))
 
     # Prepare the text caption(s)
-    # TODO 4: Create a font object with a size 28 font.
-    # TODO 5: Render the text "Two Dogs" using the font object (it's like MAKING an image).
+    # DONE 4: Create a font object with a size 28 font.
+    font1 = pygame.font.SysFont("ComicSans", 28)
+    # CONSIDER: random font?
+    # font_name = random.choice(pygame.font.get_fonts())
+    # print(font_name)
+    # font2 = pygame.font.SysFont(font_name, 48)
+    font2 = pygame.font.SysFont("Arial", 48)
+
+    # DONE 5: Render the text "Two Dogs" using the font object (it's like MAKING an image).
+    caption1 = font1.render("Two Dogs", True, BLACK)
+    caption2 = font2.render("Bark Off", True, WHITE)
 
     # Prepare the music
-    # TODO 8: Create a Sound object from the "bark.wav" file.
+    # DONE 8: Create a Sound object from the "bark.wav" file.
+    sound1 = pygame.mixer.Sound("bark.wav")
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-            # TODO 9: Play the music (bark) if there's a mouse click.
+            # DONE 9: Play the music (bark) if there's a mouse click.
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                sound1.play()
 
         # Clear the screen and set the screen background
         screen.fill(WHITE)
 
         # Draw the image onto the screen
-        # TODO 2: Draw (blit) the image onto the screen at position (0, 0)
+        # DONE 2: Draw (blit) the image onto the screen at position (0, 0)
+        screen.blit(image1, (0, 0))
 
         # Draw the text onto the screen
-        # TODO 6: Draw (blit) the text image onto the screen in the middle bottom.
+        # DONE 6: Draw (blit) the text image onto the screen in the middle bottom.
         # Hint: Commands like these might be useful..
         #          screen.get_width(), caption1.get_width(), image1.get_height()
+        screen.blit(caption1, ( (screen.get_width() - caption1.get_width())/2 ,IMAGE_SIZE-8))
 
-        # TODO 7: On your own, create a new bigger font and in white text place a 'funny' message on top of the image.
+        # DONE 7: On your own, create a new bigger font and in white text place a 'funny' message on top of the image.
+        screen.blit(caption2, (30, screen.get_height()-100))
 
         # Update the screen
         pygame.display.update()
